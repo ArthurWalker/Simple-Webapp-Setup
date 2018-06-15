13ea8639afa25d4e3f0acc48209225f8cbf800f9
ArthurWalker <phuc.phamhuu2@mail.dcu.ie>
2018-06-16 00:25:23 +0700

    Running ok

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+//import logo from './logo.svg';
+import {Route} from "react-router-dom";
+import Homepage from "./components/pages/Homepage";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+const App =() =>
+<div>
+    <Route path="/" exact component={Homepage}/>
+</div>
 
 export default App;
---
+++ src/components/pages/Homepage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import {Link} from "react-router-dom";
+
+class Homepage extends React.Component{
+    render(){
+        return(
+            <div>Hi! This is Homepage</div>
+        );
+    }
+}
+
+export default Homepage;
