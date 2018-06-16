a138f505f42b76be7385ae02cd7af3f9cb6f9b60
ArthurWalker <phuc.phamhuu2@mail.dcu.ie>
2018-06-17 00:05:19 +0700

    Add local pictures

--- src/components/pages/Homepage.js
+++ src/components/pages/Homepage.js
@@ -1,14 +1,15 @@
 import React from "react";
 import {Link} from "react-router-dom";
-import {Button} from "semantic-ui-react";
-
+import {Button,Image} from "semantic-ui-react";
 class Homepage extends React.Component{
     render(){
         return(
-            <div>Hi! This is Homepage
+            <div>
+                Hi! This is Homepage
 
+                <Image src={require('../../pics/anh1.jpg')}/>
                 
-            <Button primary>Ok</Button>
+                <Button primary>Ok</Button>
             </div>
         );
     }
